06f92da025ff746e9fac3b14e2c23b40e16ab5b4
my-name-is-D <57439775+my-name-is-D@users.noreply.github.com>
2020-07-25 19:17:15 +0100

    all the saved data

--- world/data/plotdata.py
+++ world/data/plotdata.py
@@ -26,7 +26,7 @@ def armcsv1():
     time,LshoulderP,LshoulderR = [], [], []
     LelbowY, LelbowR,LwristY =[],[],[]
     
-    with open('/home/cata/nao_ws/src/world/data/arm err0 x=186 y=25.csv') as csv_file:
+    with open('/home/cata/nao_ws/src/world/data/arm cylinder err0 x=170 y=10.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
         for row in csv_reader:
             time.append(float(row['timestamp']))
@@ -41,7 +41,7 @@ def armcsv2():
     time,LshoulderP,LshoulderR = [], [], []
     LelbowY, LelbowR,LwristY =[],[],[]
     
-    with open('/home/cata/nao_ws/src/world/data/arm err=0.0050 x=186 y=25.csv') as csv_file:
+    with open('/home/cata/nao_ws/src/world/data/arm cylinder err0.203 x=170 y=10.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
         for row in csv_reader:
             time.append(float(row['timestamp']))
@@ -87,40 +87,46 @@ def main():
     plt.plot(timehand,Lphalanx3,'-g', label='Lphalanx3/rad')
     #plt.plot(timehand,Lphalanx4,'--r', label='Lphalanx4/rad')
     #plt.plot(timehand,Lphalanx5,'--g', label='Lphalanx5/rad')
-    plt.legend(framealpha=1, frameon=False)
+    plt.legend(framealpha=1, loc='upper left', frameon=False)
+    plt.title('Grasping fingers motion ')
+    plt.ylabel('motion (rad)')
+    plt.xlabel('time (s)')
     plt.show()
     """
     plt.style.use('seaborn')
     plt.scatter(time, LshoulderP, color='blue')
-    plt.plot(time,LshoulderP,'-b', label='LShoulderPitch no noise /rad')
+    plt.plot(time,LshoulderP,'-b', label='LShoulderPitch no noise')
     plt.scatter(time, LshoulderR, color='red')
-    plt.plot(time,LshoulderR,'-r', label='LShoulderRoll no noise /rad')
+    plt.plot(time,LshoulderR,'-r', label='LShoulderRoll no noise')
 
+    
     plt.scatter(time2, LshoulderP2, color='magenta')
-    plt.plot(time2,LshoulderP2,'-m', label='LShoulderPitch with noise/rad')
+    plt.plot(time2,LshoulderP2,'-m', label='LShoulderPitch 0.203 noise')
     plt.scatter(time2, LshoulderR2, color='yellow')
-    plt.plot(time2,LshoulderR2,'-y', label='LShoulderRoll with noise/rad')
+    plt.plot(time2,LshoulderR2,'-y', label='LShoulderRoll 0.203 noise')
+    
     plt.legend(framealpha=1, loc='upper left', frameon=False)
-    plt.title('Shoulder motion with a noise of value 0.005 and without noise')
+    plt.title('Shoulder motion with and without noise ')
     plt.ylabel('motion (rad)')
     plt.xlabel('time (s)')
-    plt.xlim(0,1.6)
+    plt.xlim(0.1,5)
     plt.show()
-
+    
     plt.scatter(time, LelbowY, color='green')
-    plt.plot(time,LelbowY,'-g', label='LelbowYaw no noise/rad')  
+    plt.plot(time,LelbowY,'-g', label='LelbowYaw no noise')  
     plt.scatter(time, LelbowR, color='red') 
-    plt.plot(time,LelbowR,'-r', label='LelbowRoll no noise/rad')
-
+    plt.plot(time,LelbowR,'-r', label='LelbowRoll no noise')
+    
     plt.scatter(time2, LelbowY2, color='magenta')
-    plt.plot(time2,LelbowY2,'-m', label='LelbowYaw with noise/rad')  
+    plt.plot(time2,LelbowY2,'-m', label='LelbowYaw 0.203rad noise')  
     plt.scatter(time2, LelbowR2, color='yellow') 
-    plt.plot(time2,LelbowR2,'-y', label='LelbowRoll with noise/rad')
-    plt.legend(framealpha=1, loc='lower left', frameon=False)   
-    plt.title('Elbow motion with a noise of value 0.005 and without noise')
+    plt.plot(time2,LelbowR2,'-y', label='LelbowRoll 0.203rad noise')
+    
+    plt.legend(framealpha=1, loc='upper right', frameon=False)   
+    plt.title('Elbow motion  with and without noise ')
     plt.ylabel('motion (rad)')
     plt.xlabel('time (s)')
-    plt.xlim(0,1.6)
+    plt.xlim(0.1,5)
     plt.show() 
     
     
